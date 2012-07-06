3009e476abe102aed082645bb3fa21f02469fa6c
Richard Liu <r66033@freescale.com>
2012-07-06 16:29:20 +0800

    ENGR00215859 fix mx6sl Gallery preview picture flicker issue
    fix mx6sl Gallery preview picture flicker issue
    separate HAVE_FSL_IMX_GPU to HAVE_FSL_IMX_GPU3D and HAVE_FSL_IMX_GPU2D
    
    Signed-off-by: Richard Liu <r66033@freescale.com>

--- Android.mk
+++ Android.mk
@@ -1,4 +1,5 @@
 LOCAL_PATH:= $(call my-dir)
+ifeq ($(HAVE_FSL_IMX_GPU3D),true)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
@@ -22,3 +23,4 @@ include $(BUILD_PACKAGE)
 
 # Use the following include to make our test apk.
 include $(call all-makefiles-under,$(LOCAL_PATH))
+endif
